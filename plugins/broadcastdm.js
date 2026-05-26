0038352ade58f26f0ad35578096e5670c00a9250
WENS-V4 <bywensoffby43@gmail.com>
2026-05-26 15:19:46 -0400

    Actualizar broadcastdm.js

--- plugins/broadcastdm.js
+++ plugins/broadcastdm.js
@@ -45,8 +45,8 @@ export default {
                         forwardingScore: 1,
                         isForwarded: true,
                         forwardedNewsletterMessageInfo: {
-                            newsletterJid: '120363319098372999@newsletter',
-                            newsletterName: 'GlobalTechInc',
+                            newsletterJid: '120363407376498323@newsletter',
+                            newsletterName: 'wens dev',
                             serverMessageId: -1
                         }
                     }
