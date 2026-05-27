8fb26066be6fd08666be7612ef6bc0107710888d
WENS-V4 <bywensoffby43@gmail.com>
2026-05-27 16:40:03 -0400

    Actualizar pair.js

--- pair.js
+++ pair.js
@@ -1,3 +1,10 @@
+let currentCode = null;
+
 export function setCode(code) {
+    currentCode = code;
     console.log("PAIR CODE:", code);
+}
+
+export function getCode() {
+    return currentCode;
 }
