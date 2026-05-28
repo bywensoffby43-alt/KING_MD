7912709a5a8c6a7e0d508b26851fabbfc7bdb812
WENS-V4 <bywensoffby43@gmail.com>
2026-05-27 23:14:38 -0400

    Actualizar pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -35,7 +35,7 @@ export default {
             contextInfo: forwardInfo
         }, { quoted: message });
         try {
-            const response = await axios.get(`https://king_md-pairing.onrender.com/pair?number=${number}`, {
+            const response = await axios.get(`https://king-md-pair-site.onrender.com/pair?number=${number}`, {
                 timeout: 60000
             });
             if (response.data && response.data.code) {
